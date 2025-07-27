01b63f74a00ac7f1beb11e550d9795842d65bcd9
kimdoyun98 <rladlsxo1250@gmail.com>
2025-07-28 00:06:55 +0900

    [Refactor] 미사용 변수 제거

--- app/src/main/java/com/example/todo_list/ui/schedule/add/ScheduleRegisterActivity.kt
+++ app/src/main/java/com/example/todo_list/ui/schedule/add/ScheduleRegisterActivity.kt
@@ -1,11 +1,8 @@
 package com.example.todo_list.ui.schedule.add
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import com.example.todo_list.R
 import com.example.todo_list.databinding.ActivityRegisterBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,19 +15,5 @@ class ScheduleRegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.add_schedule_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
     }
-
-
-//    private fun getRandomColor(): Int {
-//        val range = (0..255)
-//        return Color.argb(255, range.random(), range.random(), range.random())
-//    }
-//
-//    companion object {
-//        private const val DATE_PICKER = "date picker"
-//    }
 }
