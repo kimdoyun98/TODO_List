83988280c3b4216748c616266cb09689d5c1660f
kimdoyun98 <rladlsxo1250@gmail.com>
2025-07-28 20:46:57 +0900

    [Feat] 루틴 또는 스케줄 변경 시 위젯 업데이트

--- app/src/main/java/com/example/todo_list/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/todo_list/ui/home/HomeFragment.kt
@@ -1,11 +1,18 @@
 package com.example.todo_list.ui.home
 
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import androidx.fragment.app.viewModels
 import com.example.todo_list.databinding.FragmentHomeBinding
 import com.example.todo_list.ui.util.StateDialog
 import com.example.todo_list.ui.util.basefragment.ViewBindingFragment
+import com.example.todo_list.widget.routine.RoutineWidgetProvider
+import com.example.todo_list.widget.schedule.ScheduleWidgetProvider
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class HomeFragment : ViewBindingFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
@@ -23,5 +30,53 @@ class HomeFragment : ViewBindingFragment<FragmentHomeBinding>(FragmentHomeBindin
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
         binding.fragment = this@HomeFragment
+
+        fragmentScope {
+            launch {
+                viewModel.todayRoutine.collect {
+                    routineWidgetUpdate()
+                }
+            }
+
+            launch {
+                viewModel.recentSchedule.collect {
+                    scheduleWidgetUpdate()
+                }
+            }
+        }
+    }
+
+    private fun scheduleWidgetUpdate() {
+        val intent = Intent(requireContext(), ScheduleWidgetProvider::class.java).apply {
+            setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE)
+        }
+
+        val appWidgetManager = AppWidgetManager.getInstance(requireContext())
+        val appWidgetIds = appWidgetManager.getAppWidgetIds(
+            ComponentName(
+                requireContext(),
+                ScheduleWidgetProvider::class.java
+            )
+        )
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds)
+
+        activity?.sendBroadcast(intent)
+    }
+
+    private fun routineWidgetUpdate() {
+        val intent = Intent(requireContext(), RoutineWidgetProvider::class.java).apply {
+            setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE)
+        }
+
+        val appWidgetManager = AppWidgetManager.getInstance(requireContext())
+        val appWidgetIds = appWidgetManager.getAppWidgetIds(
+            ComponentName(
+                requireContext(),
+                RoutineWidgetProvider::class.java
+            )
+        )
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds)
+
+        activity?.sendBroadcast(intent)
     }
 }
