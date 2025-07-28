c71845a678ed9ee22dd76458ddde240010e7ffae
kimdoyun98 <rladlsxo1250@gmail.com>
2025-07-28 12:11:40 +0900

    [Chore] 미사용 import  제거

--- app/src/main/java/com/example/todo_list/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/todo_list/ui/home/HomeFragment.kt
@@ -1,13 +1,7 @@
 package com.example.todo_list.ui.home
 
-import android.app.Dialog
 import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.widget.TextView
-import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.viewModels
-import com.example.todo_list.R
 import com.example.todo_list.databinding.FragmentHomeBinding
 import com.example.todo_list.ui.util.StateDialog
 import com.example.todo_list.ui.util.basefragment.ViewBindingFragment
