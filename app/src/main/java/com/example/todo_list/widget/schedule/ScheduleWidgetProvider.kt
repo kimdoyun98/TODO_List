5aa0845e744ea0922800ee81734d08c9f9878642
kimdoyun98 <rladlsxo1250@gmail.com>
2025-07-31 19:27:45 +0900

    [Feat] Schedule Widget Click 앱 실행

--- app/src/main/java/com/example/todo_list/widget/schedule/ScheduleWidgetProvider.kt
+++ app/src/main/java/com/example/todo_list/widget/schedule/ScheduleWidgetProvider.kt
@@ -1,11 +1,14 @@
 package com.example.todo_list.widget.schedule
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 import com.example.todo_list.R
 import com.example.todo_list.data.repository.schedule.ScheduleRepository
+import com.example.todo_list.ui.MainActivity
 import com.example.todo_list.util.DateCalculate
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
@@ -36,6 +39,14 @@ class ScheduleWidgetProvider : AppWidgetProvider() {
                 if (recentSchedule == null) "" else dateCalculate.getDDayString(recentSchedule.start_date)
 
             val remoteViews = RemoteViews(context?.packageName, R.layout.widget_schedule)
+            val pendingIntent = PendingIntent.getActivity(
+                context,
+                0,
+                Intent(context, MainActivity::class.java),
+                PendingIntent.FLAG_IMMUTABLE
+            )
+
+            remoteViews.setOnClickPendingIntent(R.id.schedule_layout_widget, pendingIntent)
             remoteViews.setTextViewText(R.id.schedule_title_widget, title)
             remoteViews.setTextViewText(R.id.schedule_period_date_widget, periodDay)
 
