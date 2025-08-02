3fe819cd38ba54ffe00dfce83790fc068b9acd27
kimdoyun98 <rladlsxo1250@gmail.com>
2025-08-02 21:14:09 +0900

    [Refactor] RoutineFragment Dialog 완료 변경

--- app/src/main/java/com/example/todo_list/ui/routine/RoutineViewModel.kt
+++ app/src/main/java/com/example/todo_list/ui/routine/RoutineViewModel.kt
@@ -2,18 +2,25 @@ package com.example.todo_list.ui.routine
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.todo_list.data.repository.log.RoutineLogRepository
 import com.example.todo_list.data.repository.routine.RoutineRepository
 import com.example.todo_list.data.room.RoutineEntity
+import com.example.todo_list.data.room.RoutineLog
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RoutineViewModel @Inject constructor(
-    private val repository: RoutineRepository
+    private val repository: RoutineRepository,
+    private val routineLogRepository: RoutineLogRepository,
 ) : ViewModel() {
     val getAll: StateFlow<List<RoutineEntity>> = repository.selectAll().stateIn(
         viewModelScope,
@@ -21,9 +28,25 @@ class RoutineViewModel @Inject constructor(
         emptyList()
     )
 
+    private val _todayRoutineLog = MutableStateFlow<RoutineLog?>(null)
+    private val todayRoutineLog = _todayRoutineLog.asStateFlow()
+
+    init {
+        routineLogRepository.getTodayLog()
+            .onEach { _todayRoutineLog.value = it }
+            .launchIn(viewModelScope)
+    }
+
     fun update() = viewModelScope.launch { repository.update() }
 
-    fun todaySuccess(id: Int) = viewModelScope.launch { repository.todaySuccess(id) }
+    fun todaySuccess(id: Int) = viewModelScope.launch {
+        val routines = todayRoutineLog.value?.routines?.toMutableMap() ?: return@launch
+        routines[id]?.success = true
+
+        routineLogRepository.update(
+            todayRoutineLog.value!!.copy(routines = routines)
+        )
+    }
 
     fun delete(id: Int) = viewModelScope.launch { repository.delete(id) }
 }
