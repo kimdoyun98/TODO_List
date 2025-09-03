23245112ebd4c88750f3641bb2e4210598273aaa
kimdoyun98 <rladlsxo1250@gmail.com>
2025-09-04 07:44:29 +0900

    [Refactor] application convention hilt

--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -1,13 +1,8 @@
 import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 import com.project.convention.configureAndroid
-import com.project.convention.getLibrary
-import com.project.convention.implementation
-import com.project.convention.kapt
-import com.project.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 
 class AndroidApplicationConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -15,17 +10,11 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("com.android.application")
                 apply("org.jetbrains.kotlin.android")
-                apply("kotlin-kapt")
-                apply("dagger.hilt.android.plugin")
+                apply("todo.hilt")
             }
 
             extensions.configure<BaseAppModuleExtension> {
                 configureAndroid(commonExtension = this)
-            }
-
-            dependencies {
-                implementation(libs.getLibrary("dagger.hilt.android"))
-                kapt(libs.getLibrary("hilt.compiler"))
             }
         }
     }
